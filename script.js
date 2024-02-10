e6f54d200d96b3552632e16a00daded3adecad59
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-02-10 21:35:17 +0530

    Fixed Bugs

--- script.js
+++ script.js
@@ -20,10 +20,10 @@
 let navbar2 = document.getElementById('navbar2')
 let nav_logo = document.getElementById('nav-logo');
 
-// nav_logo.onmouseover = visibility =>{
-//     navbar2.style.visibility = 'visible';
-// }
+nav_logo.onmouseover = visibility =>{
+    navbar2.style.visibility = 'visible';
+}
 
-// navbar2.onmouseleave = (e)=>{
-//     navbar2.style.visibility = 'hidden';
-// }
+navbar2.onmouseleave = (e)=>{
+    navbar2.style.visibility = 'hidden';
+}
